fad885ac9c2ec7183c3d22b9009b00237c0bf928
nviser <super_155305@ukr.net>
2017-03-09 11:55:25 +0200

    refreshing and deleting inaccuracies fixed

--- empeek/js/main.ctrl.js
+++ empeek/js/main.ctrl.js
@@ -1,19 +1,30 @@
-app.controller('mainCtrl', ['$scope', function ($scope) {
+app.controller('mainCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
     var items = JSON.parse(localStorage.getItem('items'));
     $scope.items = items;
-    $scope.curItemComments = items[0].comments;
-    $scope.id = items[0].id;
+    $scope.init = function () {
+        if ($scope.items.length === 0) {
+            $scope.curItemComments = [];
+            $scope.id = '';
+        } else {
+            $scope.curItemComments = items[0].comments;
+            $scope.id = items[0].id;
+        }
+    }
+    $scope.init();
     $scope.addItem = function () {
-        if($scope.newItem){
+        if ($scope.newItem) {
             $scope.items = actFactory().add($scope.newItem);
             $scope.newItem = '';
+            if($scope.items.length == 1){
+                $scope.details($scope.items[0]);
+            }
         }
     }
     $scope.deleteItem = function (item) {
         var isApproved = confirm('Are you sure want to delete this item?');
         if (isApproved) {
             $scope.items = actFactory().delete(item);
-            $scope.refresh($scope.items, item.id);
+            $scope.refresh($scope.items, item.id, true, item);
         }
     }
     $scope.details = function (item) {
@@ -21,13 +32,22 @@ app.controller('mainCtrl', ['$scope', function ($scope) {
         $scope.id = item.id;
     }
     $scope.addComment = function (id) {
-        if($scope.newComment){
+        if ($scope.newComment) {
             $scope.items = actFactory().newComment(id, $scope.newComment);
-            $scope.refresh($scope.items, id);
+            $scope.refresh($scope.items, id, false);
             $scope.newComment = '';
         }
     }
-    $scope.refresh = function (arg, id) {
+    $scope.refresh = function (arg, id, isDel, item) {
+        if (!arg.length) {
+            $scope.curItemComments = [];
+            $scope.id = '';
+            return;
+        }
+        if(isDel && $scope.id == item.id) {
+            $scope.details($scope.items[0]);
+            return;
+        }
         angular.forEach(arg, function (val, key) {
             if (val.id == id) {
                 $scope.curItemComments = arg[key].comments;
